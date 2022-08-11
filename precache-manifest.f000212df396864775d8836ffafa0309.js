682dd28b528498c0b5aaee042e6149facae1f25a
e.trachuk <e.trachuk@priem.com>
2022-08-11 15:01:41 +0300

    Updates

---
+++ precache-manifest.f000212df396864775d8836ffafa0309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cce9006a8042af51776",
+    "url": "/simple-trello-clone/static/css/main.dfa35b5f.chunk.css"
+  },
+  {
+    "revision": "5cce9006a8042af51776",
+    "url": "/simple-trello-clone/static/js/main.5cce9006.chunk.js"
+  },
+  {
+    "revision": "33c32870a225fcdc2f36",
+    "url": "/simple-trello-clone/static/js/runtime~main.33c32870.js"
+  },
+  {
+    "revision": "54afe5dd53df65d76e08",
+    "url": "/simple-trello-clone/static/js/2.54afe5dd.chunk.js"
+  },
+  {
+    "revision": "a98ff699d97d5b2849714b8396692279",
+    "url": "/simple-trello-clone/index.html"
+  }
+];
